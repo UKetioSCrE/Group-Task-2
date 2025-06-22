c9ca4eeb71a7a8118460a09b08a32b8824d1bb98
wania <waniat1@umbc.edu>
2025-06-22 01:39:15 -0400

    need multiplication.java

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SimpleCalculator {
     public static void main(String[] args) {
         int a = 10;
@@ -7,7 +5,7 @@ public class SimpleCalculator {
 
         System.out.println("Addition: " + add(a, b));
         System.out.println("Subtraction: " + subtract(a, b));
-        System.out.println("Multiplication: " + multiply(a, b));
+        //System.out.println("Multiplication: " + multiply(a, b));
     }
 
 
